be153e6ea38f8cdde3eaf283c3849f37974a5f32
zhu00084 <zhu00084@algonquinlive.com>
2017-02-27 10:36:42 -0500

    Finished lab5

--- app/src/main/java/com/example/qiu/androidlabs/ChatWindow.java
+++ app/src/main/java/com/example/qiu/androidlabs/ChatWindow.java
@@ -1,5 +1,6 @@
 package com.example.qiu.androidlabs;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -13,14 +14,77 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
-
+import android.util.Log;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import java.util.ArrayList;
+
+
 
 public class ChatWindow extends AppCompatActivity {
 
-    private class ChatAdapter extends ArrayAdapter<String> {
+    Button button4;
+    EditText editText;
+    ListView chatList;
+    ArrayList<String> messages = new ArrayList<String>();
+    protected static final String ACTIVITY_NAME = "ChatWindow";
+    protected ChatDatabaseHelper dbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_window);
+        dbHelper = new ChatDatabaseHelper(this);
+
+        final SQLiteDatabase db = dbHelper.getWritableDatabase();
+        Cursor results = db.query(false, ChatDatabaseHelper.DATABASE_NAME,
+                new String[]{ChatDatabaseHelper.KEY_ID, ChatDatabaseHelper.KEY_MESSAGE},null, null, null, null, null, null);
+        int rows = results.getCount();
+        results.moveToFirst();
+        while(!results.isAfterLast()) {
+           messages.add(results.getString(results.getColumnIndex(ChatDatabaseHelper.KEY_MESSAGE)));
+            Log.i(ACTIVITY_NAME, "SQL MESSAGE:" + results.getString( results.getColumnIndex( ChatDatabaseHelper.KEY_MESSAGE) ) );
+            results.moveToNext();
+        }
+
+        button4 = (Button) findViewById(R.id.btnSend);
+        editText = (EditText) findViewById(R.id.editText4);
+        chatList = (ListView) findViewById(R.id.ChatList);
+
+        final ChatAdapter messageAdapter = new ChatAdapter(this);
+        chatList.setAdapter(messageAdapter);
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String newMessage = editText.getText().toString();
+                messages.add(newMessage);
+
+                messageAdapter.notifyDataSetChanged();
+
+
+                ContentValues values = new ContentValues();
+                values.put(ChatDatabaseHelper.KEY_MESSAGE, editText.getText().toString());
+                db.insert(ChatDatabaseHelper.DATABASE_NAME, "", values);
+
+                editText.setText("");
+
+            }
+        });
+
+        Log.i(ACTIVITY_NAME, "Cursor’s  column count =" + results.getColumnCount() );
+       for (int i=0; i< results.getColumnCount(); i++){
+           System.out.println(results.getColumnName(i));
+        }
+    }
+
+
+    public class ChatAdapter extends ArrayAdapter<String> {
+
         public ChatAdapter(Context ctx){
             super(ctx,0);
+
         }
 
 
@@ -56,36 +120,13 @@ public class ChatWindow extends AppCompatActivity {
             message.setText(getItem(position));
             return result;
         }
-    };
+    }
 
-    Button button4;
-    EditText editText;
-    ListView chatList;
 
-    ArrayList<String> messages = new ArrayList<String>();
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat_window);
-
-        button4 = (Button) findViewById(R.id.btnSend);
-        editText = (EditText) findViewById(R.id.editText4);
-        chatList = (ListView) findViewById(R.id.ChatList);
-
-        final ChatAdapter messageAdapter = new ChatAdapter(this);
-        chatList.setAdapter(messageAdapter);
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                String newMessage = editText.getText().toString();
-                messages.add(newMessage);
-
-                messageAdapter.notifyDataSetChanged();
-                editText.setText("");
-            }
-        });
+    protected void onDestroy() {
+        super.onDestroy();
+        dbHelper.close();
+        Log.i(ACTIVITY_NAME, "In onDestroy()");
     }
 
 
